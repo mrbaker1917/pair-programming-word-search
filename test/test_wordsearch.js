74f7eb76a07962b31537fab0e782641b910cc3f0
Mark Baker <mrbaker1917@gmail.com>
2020-08-27 00:58:04 +0000

    Adds completed wordsearch.js and test file

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,7 +31,55 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['M', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['M', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['R', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['S', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['U', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'AMMARSIOU');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards", function() {
+    const result = wordSearch([
+      ['T', 'S', 'E', 'B', 'K', 'R', 'A', 'M'],
+      ['M', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['M', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['R', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['S', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['U', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'MARKBEST');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertical backwards", function() {
+    const result = wordSearch([
+      ['T', 'S', 'E', 'B', 'K', 'R', 'Z', 'M'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['E', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['B', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['S', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['M', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['A', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['J', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JAMESBEST');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  const backwardsJoin = letters.map(ls => ls.reverse().join(''));
+  for (let l of backwardsJoin) {
+    if (l.includes(word)) return true;
+  }
+  const transposedMatrix = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let tempArr = [];
+    for (let m = 0; m < letters.length; m++) {
+      tempArr.push(letters[m][i]);
+    }
+    transposedMatrix.push(tempArr);
+  }
+  const verticalJoin = transposedMatrix.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalBack = transposedMatrix.map(ls => ls.reverse().join(''));
+  for (let l of verticalBack) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+module.exports = wordSearch;
+
+//diagonal - look through original matrix at [i=0]][m=0] i++ if letter is the first letter of our search...
+//...then execute diagonal search in upward or downward
+//OPtion 2: regex positive lookahead?? --
